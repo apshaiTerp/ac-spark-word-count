c55238703985dcdddc6226b61a1d27291bcc7980
apshaiTerp <apshaiTerp@yahoo.com>
2016-09-07 14:57:43 -0500

    Trying new update

--- src/main/java/com/ac/umkc/pbd/BasicSparkWordCount.java
+++ src/main/java/com/ac/umkc/pbd/BasicSparkWordCount.java
@@ -2,15 +2,15 @@ package com.ac.umkc.pbd;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.regex.Pattern;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.sql.SparkSession;
 
 import scala.Tuple2;
 
@@ -41,16 +41,20 @@ public class BasicSparkWordCount {
    */
   @SuppressWarnings("serial")
   public static void main(String[] args) {
-    if (args.length < 1) {
-      System.err.println ("No file path provided as $1 input");
+    if (args.length < 2) {
+      System.err.println ("You did not specify the input and output file names");
       return;
     }
     
     try {
-      SparkSession spark = SparkSession.builder().appName("JavaWordCount").getOrCreate();
-      JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-
-      JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+      SparkConf sparkConfiguration = new SparkConf();
+      sparkConfiguration.setAppName("Basic Spark Word Count");
+      sparkConfiguration.setMaster("local");
+      
+      JavaSparkContext sparkContext = new JavaSparkContext(sparkConfiguration);
+      JavaRDD<String> messageFile   = sparkContext.textFile(args[0]).cache();
+      
+      JavaRDD<String> words = messageFile.flatMap(new FlatMapFunction<String, String>() {
           public Iterator<String> call(String s) {
             return Arrays.asList(SPACE.split(s)).iterator();
           }
@@ -68,12 +72,9 @@ public class BasicSparkWordCount {
           }
       });
 
-      List<Tuple2<String, Integer>> output = counts.collect();
-      for (Tuple2<?,?> tuple : output) {
-        System.out.println(tuple._1() + ": " + tuple._2());
-      }
+      counts.saveAsTextFile(args[1]);
       
-      spark.stop();      
+      sparkContext.close();
       
     } catch (Throwable t) {
       System.err.println ("Something bad happened: " + t.getMessage());
